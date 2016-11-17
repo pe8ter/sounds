dc9c6afb3cb33f3343c6dcd13ee6442d348418bc
Peter Safranek <pe8ter@mac.com>
2016-11-17 08:55:02 -0800

    Add Play/Pause button.

--- src/components/sounds-app/sounds-app.ts
+++ src/components/sounds-app/sounds-app.ts
@@ -1,10 +1,28 @@
 
 import { Component } from '@angular/core';
 
+const PLAY = 'Play';
+const PAUSE = 'Pause';
+
 @Component({
     moduleId: module.id,
     selector: 'sounds-app',
     styleUrls: ['sounds-app.css'],
     templateUrl: 'sounds-app.html',
 })
-export class SoundsApp {}
+export class SoundsApp {
+
+    public playPauseTitle = PLAY;
+
+    public onPlayPauseClick(): void {
+        if (this._playing) {
+            this._playing = false;
+            this.playPauseTitle = PLAY;
+        } else {
+            this._playing = true;
+            this.playPauseTitle = PAUSE;
+        }
+    }
+
+    private _playing = false;
+}
